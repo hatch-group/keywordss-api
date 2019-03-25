0a2b6d90c1a38de98cfbef050319eb5731b8a41c
tea1013 <tea.music.prog@gmail.com>
2019-03-25 17:37:20 +0900

    MYSQL用の環境変数を設定するように実装

--- api/main.go
+++ api/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	fmt.Println(os.Getenv("MYSQL_USER"))
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
