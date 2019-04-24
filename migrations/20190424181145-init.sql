c6ab6c68b2db2a45fb818d94d6f2f89427046806
tea1013 <tea.music.prog@gmail.com>
2019-04-24 18:23:04 +0900

    migration環境の構築

---
+++ migrations/20190424181145-init.sql
@@ -0,0 +1,50 @@
+-- +migrate Up
+
+CREATE TABLE users (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (name)
+);
+
+CREATE TABLE stories (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  status INT NOT NULL,
+  created DATETIME NOT NULL,
+  updated DATETIME NOT NULL,
+  count INT NOT NULL,
+  user_id BIGINT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (title(255), body(255)),
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+
+CREATE TABLE keywords (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  word VARCHAR(255) NOT NULL,
+  type INT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (word)
+);
+
+CREATE TABLE likes (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  user_id BIGINT NOT NULL,
+  story_id BIGINT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id)
+    REFERENCES users(id),
+  FOREIGN KEY (story_id)
+    REFERENCES stories(id)
+);
+
+-- +migrate Down
+
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS keywords;
+DROP TABLE IF EXISTS stories;
+DROP TABLE IF EXISTS users;
+
