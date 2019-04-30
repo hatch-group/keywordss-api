7daa4e0832171d2bf6d8c0951441829b6e35d446
佐藤大介 <workshift56@gmail.com>
2019-04-30 23:30:29 +0900

    変なコメント削除

--- api/main.go
+++ api/main.go
@@ -10,42 +10,6 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-// type Server struct {
-// 	db *sqlx.DB
-// }
-
-// func (s *Server) init() {
-// 	dburl := os.Getenv("MYSQL_URL")
-// 	db, err := sqlx.Connect("mysql", dburl)
-// 	if err != nil {
-// 		fmt.Println("mysql connect error")
-// 	}
-// 	s.db = db
-// }
-
-// func (s *Server) route() {
-// 	r := gin.Default()
-
-// 	api := r.Group("/api")
-// 	{
-// 		story := &controller.Story{}
-// 		user := &controller.User{}
-// 		story.DB = s.db
-// 		user.DB = s.db
-// 		api.GET("/stories", story.IndexGet)
-// 		api.GET("/stories/:id", story.ShowItem)
-// 		api.POST("/stories", story.Post)
-// 		api.PUT("/stories", story.Edit)
-// 		api.DELETE("/stories", story.Delete)
-
-// 		api.GET("/user/:user_id/stories", story.IndexMyPost)
-// 		api.POST("/users/signup", user.UserSignUp)
-// 		api.POST("/users/signin", user.UserSignIn)
-// 	}
-
-// 	r.Run(":8080")
-// }
-
 func main() {
 	r := gin.Default()
 	dburl := os.Getenv("MYSQL_URL")
